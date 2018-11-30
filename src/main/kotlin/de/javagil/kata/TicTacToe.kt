864cea94fc36204737d3848b64d1f19323953b70
Michael Hoennig <michael@hoennig.de>
2018-11-30 11:06:40 +0100

    `set X at free position 1, 1` - refactor

--- src/main/kotlin/de/javagil/kata/TicTacToe.kt
+++ src/main/kotlin/de/javagil/kata/TicTacToe.kt
@@ -4,5 +4,5 @@ class TicTacToe(val board: String = "··· ··· ···") {
 
     override fun toString() = board
 
-    fun setX(row: Int, col: Int) = if (row == 0) TicTacToe("X·· ··· ···") else if (col == 2) TicTacToe("··· ··X ···") else TicTacToe(board.replaceRange(4*row+col, 4*row+col+1, "X"))
+    fun setX(row: Int, col: Int) = TicTacToe(board.replaceRange(4*row+col, 4*row+col+1, "X"))
 }
