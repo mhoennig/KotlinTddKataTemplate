a44ea8dbca0c9488fff8a0b54d6ebd8adf63303a
Michael Hoennig <michael@hoennig.de>
2018-11-30 12:18:44 +0100

    `make move at free position` with Player.O - refactor

--- src/main/kotlin/de/javagil/kata/TicTacToe.kt
+++ src/main/kotlin/de/javagil/kata/TicTacToe.kt
@@ -4,12 +4,11 @@ class TicTacToe(val board: String = "··· ··· ···") {
 
     override fun toString() = board
 
-    fun move(x: Player, row: Int, col: Int) = TicTacToe(board.replaceRange(atPosition(row, col), x.name))
+    fun move(player: Player, row: Int, col: Int) = TicTacToe(board.replaceRange(atPosition(row, col), player.name))
 
     private fun atPosition(row: Int, col: Int) = position(row, col)..position(row, col)
     private fun position(row: Int, col: Int) = 4 * row + col
 }
-
 
 enum class Player {
     X, O
